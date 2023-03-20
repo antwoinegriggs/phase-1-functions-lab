f26d8cc21ad9a9406ebf7c52a52d1dd52a12dfc1
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-03-20 17:10:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const toFeet = 264;
+
+function distanceFromHqInBlocks(pickupLocation) {
+  const locationHQ = 42;
+  return Math.abs(pickupLocation - locationHQ);
+}
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * toFeet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  //returns the number of feet traveled
+  return Math.abs(start - destination) * toFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+  //returns the fare for the customer
+  const freeFareDistance = 400;
+
+  const distance = distanceTravelledInFeet(start, destination);
+
+  const price = 0.02;
+  const cost = (distance - freeFareDistance) * price;
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance < 2000) {
+    return cost;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
+
+calculatesFarePrice(34, 32);
